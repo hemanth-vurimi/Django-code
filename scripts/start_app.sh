547e9e7288a24fcef086a245aa5347e3382bdfa4
Ubuntu <ubuntu@ip-172-31-10-239.ap-south-1.compute.internal>
2025-07-25 07:03:16 +0000

    Add stop_app.sh and rename start_server.sh to start_app.sh

---
+++ scripts/start_app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source /home/ubuntu/Django-code/venv/bin/activate
+cd /home/ubuntu/Django-code
+
+# Optional: Kill previous process running on port 8000 (use with caution in prod)
+fuser -k 8000/tcp
+
+# Start Django with nohup
+nohup python manage.py runserver 0.0.0.0:8000 > server.log 2>&1 &
+
---
+++ scripts/stop_app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Stopping Django app..."
+pkill -f gunicorn
